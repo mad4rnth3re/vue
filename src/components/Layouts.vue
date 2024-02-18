d1cd165161a3e383e236d8cd16ce06e717e7d38f
mad4renth3re <opojaretakdir@gmail.com>
2024-02-18 20:57:39 +0700

    add multi role

--- src/components/Layouts.vue
+++ src/components/Layouts.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark position-absolute top-0 start-0 " style="width: 100vw; height: 15vh;">
         <div class="container-fluid">
-          <router-link class="navbar-brand" to="/">Vue</router-link>
+          <router-link class="navbar-brand" to="/">Admin</router-link>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
